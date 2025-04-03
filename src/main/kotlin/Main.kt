13fb60521bd8be568784428813a2efe52f8b8d92
Miguel <miguel.sdasilva00@gmail.com>
2025-04-03 11:25:24 -0300

    Main Class changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,19 @@
 package com.miguelsantos
 
-import java.net.URI
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse.BodyHandlers
-
+import com.google.gson.Gson
+import com.miguelsantos.model.Game
+import com.miguelsantos.model.Info
+import com.miguelsantos.services.ApiConsumer
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    val apiConsumer = ApiConsumer()
+    val json = apiConsumer.obtainData()
 
-    val client: HttpClient = HttpClient.newHttpClient()
-    val request = HttpRequest.newBuilder()
-        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
-        .build()
-    val response = client
-        .send(request, BodyHandlers.ofString())
+    val gson = Gson()
+    val game_info = gson.fromJson(json, Info::class.java)
+    val game = Game(game_info.info.title, game_info.info.thumb)
 
-    val json = response.body()
-    println(json)
-
-
+    println(game)
 }
